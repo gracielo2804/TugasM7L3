d6ce006edf1a86f71353c258a01999fd12a9e734
Gracielo Justine Santoso <gracielo1@mhs.stts.edu>
2022-11-10 21:22:16 +0700

    Activity

--- app/src/main/java/com/example/tugasm7/ui/activity/MuridActivity.kt
+++ app/src/main/java/com/example/tugasm7/ui/activity/MuridActivity.kt
@@ -7,11 +7,19 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.tugasm7.R
 import com.example.tugasm7.databinding.ActivityMuridBinding
+import com.example.tugasm7.ui.fragment.MuridListFragment
+import com.example.tugasm7.ui.fragment.MuridPilihFragment
 
 class MuridActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMuridBinding
+    var usernamelogin:String=""
+    var namalogin:String=""
+
+    lateinit var muridListFragment: MuridListFragment
+    lateinit var muridPilihFragment: MuridPilihFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +27,32 @@ class MuridActivity : AppCompatActivity() {
         binding = ActivityMuridBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if(intent.hasExtra("usernamelogin")){
+            usernamelogin=intent.getStringExtra("usernamelogin").toString()
+            namalogin=intent.getStringExtra("namalogin").toString()
+        }
+
+        muridListFragment= MuridListFragment(usernamelogin,namalogin)
+        muridPilihFragment=MuridPilihFragment(usernamelogin,namalogin)
+
+        binding.txtWelcomeMurid.text="Selamat Datang $namalogin !"
+
         val navView: BottomNavigationView = binding.navView
+        navView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item_kelas_murid ->{
+                    //ToDo Do Something
+                    true
+                }
+                R.id.item_pilih_murid ->{
+                    //ToDo Do Something
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/tugasm7/ui/activity/PengajarActivity.kt
+++ app/src/main/java/com/example/tugasm7/ui/activity/PengajarActivity.kt
@@ -7,11 +7,21 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.tugasm7.R
 import com.example.tugasm7.databinding.ActivityPengajarBinding
+import com.example.tugasm7.ui.fragment.MuridListFragment
+import com.example.tugasm7.ui.fragment.MuridPilihFragment
+import com.example.tugasm7.ui.fragment.PengajarAddFragment
+import com.example.tugasm7.ui.fragment.PengajarListFragment
 
 class PengajarActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPengajarBinding
+    var usernamelogin:String=""
+    var namalogin:String=""
+
+    lateinit var pengajarAddFragment: PengajarAddFragment
+    lateinit var pengajarListFragment: PengajarListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +29,31 @@ class PengajarActivity : AppCompatActivity() {
         binding = ActivityPengajarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if(intent.hasExtra("usernamelogin")){
+            usernamelogin=intent.getStringExtra("usernamelogin").toString()
+            namalogin=intent.getStringExtra("namalogin").toString()
+        }
+
+        pengajarAddFragment=PengajarAddFragment(usernamelogin,namalogin)
+        pengajarListFragment=PengajarListFragment(usernamelogin,namalogin)
+
+        binding.txtWelcomePengajar.text="Selamat Datang $namalogin !"
         val navView: BottomNavigationView = binding.navView
 
+        navView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item_kelas_pengajar ->{
+                    //ToDo Do Something
+                    true
+                }
+                R.id.item_tambah_pengajar ->{
+                    //ToDo Do Something
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
     }
 }
